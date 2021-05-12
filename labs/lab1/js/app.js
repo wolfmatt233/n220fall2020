f5595409e6e65b1cf337637c7fe8bba85071e47e
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-05-12 18:58:32 -0400

    Creation of Personal Composition 1
    Set up scripts and did P5 work.

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,34 @@
+function setup() {
+
+    //Variable to use for every planet to keep them all in the middle
+    var midY = 300;
+
+    createCanvas (1585, 600);
+    noStroke();
+
+    background(0, 0, 0)
+    
+    //Sun
+    fill(235, 137, 52)
+    stroke(245, 206, 49)
+    strokeWeight(3)
+    circle(0, 300, 175, 175);
+    
+    //mercury
+    fill(209, 189, 159)
+    stroke (61, 59, 56)
+    circle(215, midY, 15, 15)
+
+    //Venus
+    fill(232, 167, 46)
+    circle(290, midY, 37, 37)
+
+    //Earth
+    fill(47, 128, 31)
+    circle(400, midY, 40, 40)
+
+    //Mars
+    fill(212, 86, 13)
+    circle(500, midY, 20, 20)
+
+}
