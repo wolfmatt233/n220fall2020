f9b872a683c6a514e66b9372b5c2bd5891bbcb8d
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-05-18 12:41:01 -0400

    Edited titles in main index. Finished option 5 for lab 2.

--- labs/lab2(3)/js/app.js
+++ labs/lab2(3)/js/app.js
@@ -1,18 +1,26 @@
-let s = 20;
+//variable for the starting circle size
+let c = 1;
+  console.log(c)
 
+//canvas creation
 function setup() {
-    createCanvas(500, 500)
+    createCanvas(600, 600)
 }
 
-
+//background and circle setup
 function draw() {
   background(0,0,0)
-  circle(mouseX, mouseY, s);
+  circle(300, 300, c);
 
-  if(s < 50) {
-    if (mouseIsPressed) {
-        s = s + 5; 
-        console.log(s);
+  //if c is less than 200, then increase the size by one
+  if(c < 200) {
+    c = c + 1; 
+      console.log(c);
   }
- }  
-}
+
+  //otherwise go back to 1 (if the circle exceeds 200 reset the circle to 1)
+  else {
+    c = 1
+      console.log(c)
+  }
+ }
--- labs/lab2(3)/js/plan.js
+++ labs/lab2(3)/js/plan.js
@@ -1,3 +1,9 @@
+//set up global variable for circle size
+
 //set up canvas
 
-//
+//set up circle in middle of canvas along with background and any other styling choices
+    //connect variable to circle
+
+//If the circle is less than 200, then increase the circle diameter by 1.
+    //Otherwise reset the circle to 1. (If the circle reaches 200 reset the circle diameter to 1.)
