ec6110d1ef043a454c481c0414f42dd8e8f24836
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-05-23 19:24:11 -0400

    Finished Lab 3 part 3, touched up on algorithm planning, added names

--- labs/Lab3/Lab3(1)/js/app.js
+++ labs/Lab3/Lab3(1)/js/app.js
@@ -28,3 +28,21 @@ function draw() {
         }
     }
 }
+
+//Algorithm Plan
+
+//setup canvas and background
+
+//Set x equal to 16. Add 32 
+//If x is less than the width of the canvas, 
+    //then create a circle. 
+    //Loop this until x is greater than the width of the canvas
+
+    //if x is divisible by 3 with 0 remainders
+        //then fill the circle with purple
+
+    //if x is divisible 5 with 0 remainders
+        //then draw a square and fill it with green 
+
+    //if x is divisible by 3 with 0 remainders AND if x is divisible by 5 with 0 remainders
+        //then draw a square and fill it with blue
--- labs/Lab3/Lab3(3)/js/app.js
+++ labs/Lab3/Lab3(3)/js/app.js
@@ -1,7 +1,18 @@
 function setup() {
-    createCanvas(400, 300)
+    createCanvas(400, 400)
 }
 
 function draw() {
-    
+    //Canvas color, nofill for circle outlines
+    background(200)
+    noFill()
+
+    //For loop for circles
+    //let i = 0; if i is less than 40
+    for(let i = 0; i < 50; i++) { //add 1 to i every loop  
+        
+        // then create a circle at position 200, 200 with an size of i*4
+        //i*4 controls the space in between each circle
+        circle(200, 200, i*4)
+    }
 }
