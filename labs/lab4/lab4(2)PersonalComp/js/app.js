60ff28d8110ecb93a4ee5932d88b68c5d24c98a8
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-05-27 02:19:44 -0400

    Touchups and links added

--- labs/lab4/lab4(2)PersonalComp/js/app.js
+++ labs/lab4/lab4(2)PersonalComp/js/app.js
@@ -1,3 +1,4 @@
+//declaring color array
 let myColors = ["#ff0000", "#ff5100", "#ffd000", "#00ff15", "#00f7ff", "#002fff", "#8800ff", "#ff00c3"]
 
 function setup() {
@@ -7,14 +8,14 @@ function setup() {
 function draw() {
     background(0)
 
+    //line of circles horizontal
     for (let i = 0; i < myColors.length; i++) {
         fill(myColors[i])
         circle(i * 50 + 30, 205, 50)
     }
+    //line of circles vertical
     for (let i = 0; i < myColors.length; i++) {
         fill(myColors[i])
         circle(205, i * 50 + 30, 50)
     }
 }
-
-//Algorithm Plan\\
