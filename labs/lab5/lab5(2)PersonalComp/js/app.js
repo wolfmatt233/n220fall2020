98fec391e0dae1b195ec6031e48767f9942fdb67
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-05-31 21:14:59 -0400

    Lab5 janky but works i guess

--- labs/lab5/lab5(2)PersonalComp/js/app.js
+++ labs/lab5/lab5(2)PersonalComp/js/app.js
@@ -1,16 +1,39 @@
-let dvGridSquares = document.getElementById("mygrid")
-//Array for squares
-let divArray = []
+let dvSquare = document.getElementById("mySquare")
 
-//Random colors
+//Messages and color arrays
+let message1 = "Primary!"
+let message2 = "Secondary!"
+let primaryColor = ["#FF0000", "#FFFF00", "#3559e8"]
+let secondaryColor = ["#FFA500", "#7F00FF", "#00FF00"]
+
+//Random calculations for colors
+let randColorPri = Math.floor(Math.random() * primaryColor.length)
+let randColorSec = Math.floor(Math.random() * secondaryColor.length)
+
+//Styling: backgrund, dimensions
+dvSquare.style.backgroundColor = "gray";
+dvSquare.style.height = "250px";
+dvSquare.style.width = "250px";
+
+//Centers text horizontally and vertically to div
+dvSquare.style.textAlign = "center";
+dvSquare.style.verticalAlign = "middle";
+dvSquare.style.lineHeight = dvSquare.style.height
+dvSquare.innerHTML = "Flip a coin and guess my color."
 
 
-dvGridSquares.style.float = "left"
-dvGridSquares.style.height = "20px"
-dvGridSquares.style.width = "20px"
-dvGridSquares.style.backgroundColor = "randColors"
-dvGridSquares.style.margin = "10px"
+function squareChange() {
+    //Repurposed coin flip to allow multiple clicks
+    let flip = Math.random()
+    let side = "heads"
+    if(flip > .5) side = "tails"
 
-for(let i = 0; i < 10; i++) {
-    dvGridSquares
+    //if the coin flip goes to tails then display a primary color with the message "Primary!" 
+    if(side == "tails") { 
+        dvSquare.innerHTML = message1
+        dvSquare.style.backgroundColor = primaryColor[randColorPri]  
+    } else { //otherwise display a secondary color with the message "Secondary!"
+        dvSquare.innerHTML = message2
+        dvSquare.style.backgroundColor = secondaryColor[randColorSec]
+    }
 }
