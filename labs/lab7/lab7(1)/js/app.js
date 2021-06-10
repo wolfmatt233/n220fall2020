2ef706e3b0aa7f00cdaf8d95e7d5f918c9774fc7
wolfmatt233 <84036287+wolfmatt233@users.noreply.github.com>
2021-06-10 18:31:00 -0400

    Update app.js

--- labs/lab7/lab7(1)/js/app.js
+++ labs/lab7/lab7(1)/js/app.js
@@ -7,7 +7,7 @@ function calcTip() {
     let total = Number(numberInput.value * 1.15) //total + tip calc
 
     //Display results in separate div
-    dvResult.innerHTML = "Tip Amount: " + "$" + tip + "<br>" + "Total Amount: " + "$" + total
+    dvResult.innerHTML = "Tip Amount: $" + tip + "<br>" + "Total Amount: $" + total
 
     //Log tip and total calculations to console
     console.log("Tip: $", tip, "Total: $", total)
